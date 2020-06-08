56cbf3141163696c2f51b00c423f270d65bcc8a7
testuser1 <testuser1@ip-10-1-1-100.ec2.internal>
2020-06-08 02:03:55 +0000

    Etited by testuser1

--- details.sh
+++ details.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#gives Hostname
 echo $(hostname)
+#gives user executed this script
 echo $(whoami)
-echo $(id -un)
+#gives users in the system
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
